ae67ea37dc4f13d103d87b3e9bc92e0e56bb33f9
nick <snickholas@gmail.com>
2018-01-12 16:00:20 -0800

    upd before leaving wrak

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -17,12 +17,21 @@ Including another URLconf
 
 from django.urls import path
 from django.contrib import admin
-
 # Use include() to add URLS from the catalog application and authentication system
 from django.urls import include
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+
+"""urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
@@ -44,4 +53,6 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-]
+] """
+
+
